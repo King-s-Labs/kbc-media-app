93524dfec71491e42ed258535344709380ea86f0
jpham658 <jpham658@gmail.com>
2024-02-24 22:24:26 +0000

    Remove commented out code

--- src/components/media-associate/AssignedPostCard.jsx
+++ src/components/media-associate/AssignedPostCard.jsx
@@ -20,24 +20,6 @@ const AssignedPostCard = ({ name, department, post_time, time_sent_at, due_date,
             showPostTime={true}
             showAssignedTo={false}
         />
-        // <Card mode="outlined"
-        //     style={styles.cardStyle}>
-        //     <Card.Content style={styles.cardContent}>
-        //         <View style={styles.content}>
-        //             <View style={styles.textContainer}>
-        //                 <Text style={styles.titleStyle} variant="titleLarge">{name}</Text>
-        //                 <Text variant="bodyLarge">Requested by {department}</Text>
-        //                 <Text variant="bodyLarge">{
-        //                     post_time ? 
-        //                     `Scheduled for ${formatDate(post_time)}`
-        //                     : `Due for ${formatDate(due_date)}`
-        //                 }</Text>
-        //                 <Text variant="bodyLarge">{approved ? "Approved" : "Pending approval"}</Text>
-        //             </View>
-        //             <PostTypeIcon post_type={post_type} />
-        //         </View>
-        //     </Card.Content>
-        // </Card>
     )
 };
 
